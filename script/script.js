63abf3783a8e906f26eae93717e0a040a7b915e7
Ajinath-Game <gameajinath4@gmail.com>
2025-08-07 14:12:19 +0530

    Create heros section

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+
+const loadCategories = async () => {
+    try {
+        let response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+        let data = await response.json()
+        console.log(data.categories)
+    } catch (error) {
+
+    }
+}
+
+loadCategories()
